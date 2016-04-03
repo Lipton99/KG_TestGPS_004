3e073225153f7facab689e0610e1a342e3303228
Lipton99 <obake.calibur09@gmail.com>
2016-04-03 18:23:18 +0900

    gps apk first commit

---
+++ app/src/main/java/com/example/tsubasa/kg_testgps_004/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.tsubasa.kg_testgps_004;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager locationManager;    // ロケーションマネージャ
+    private TextView latitude_Label;
+    private TextView longitude_Label;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ロケーションマネージャのインスタンスを取得する
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        latitude_Label = (TextView) findViewById(R.id.latitude_Label);
+        longitude_Label = (TextView) findViewById(R.id.longitude_Label);
+    }
+    @Override
+    protected void onResume() {
+        if(locationManager != null)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        super.onResume();
+        }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 位置情報の更新を受け取るように設定
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+    }
+    @Override
+    protected void onPause() {
+        if(locationManager != null) {
+            locationManager.removeUpdates(this);
+            }
+        super.onPause();
+        }
+    @Override
+    public void onStop() {
+        super.onStop();
+        // 位置情報の更新を止める
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    // 例としてラベルに取得した位置を表示
+        latitude_Label.setText(Double.toString(location.getLatitude()));
+        longitude_Label.setText(Double.toString(location.getLongitude()));
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+}
